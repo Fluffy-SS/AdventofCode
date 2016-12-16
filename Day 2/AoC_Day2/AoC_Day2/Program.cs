cadf47d268d112cc1224790e8d90187b5a72cb5d
Fluffy-SS <seanspalding91@gmail.com>
2016-12-16 11:54:27 +0000

    added day 2 folder, reading in input from file, works correctly.

---
+++ Day 2/AoC_Day2/AoC_Day2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // direction input puzzle
+            // Read the file as one string.
+            string input = System.IO.File.ReadAllText(@"C:\Users\Sean Spalding\Documents\AdventofCode\Day 2\AoC_Day2\puzzle input.txt");
+            Console.WriteLine(input);
+            Console.Read();
+        }
+    }
+}
