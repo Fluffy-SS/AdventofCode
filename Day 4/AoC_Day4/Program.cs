5c0537c9d5de0dad799b846036a318e51ffdb9f0
Fluffy-SS <seanspalding91@gmail.com>
2017-01-05 21:38:51 +0000

    day 4 part a complete

--- Day 4/AoC_Day4/Program.cs
+++ Day 4/AoC_Day4/Program.cs
@@ -10,6 +10,27 @@ namespace AoC_Day4
     {
         static void Main(string[] args)
         {
+            // Sum of the sector IDs of the real rooms
+            int totalsum = 0;
+
+            // Load file in memory
+            string[] alllines = System.IO.File.ReadAllLines(@"C: \Users\Sean Spalding\Documents\AdventofCode\Day 4\puzzle input.txt");
+
+            // Loop through all lines
+            foreach (string line in alllines)
+            {
+                // Create room
+                Room room = new Room(line);
+
+                // If the room is valid add the sector id
+                if (room.IsValidRoom())
+                    totalsum += room.SectorId;
+            }
+
+            // Output
+            Console.WriteLine("The total sum is: {0}", totalsum);
+
+            // Keep the console open
             Console.Read();
         }
     }
